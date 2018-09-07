25e8cb5960ea5b07eff482d93ca0b5851eb24d09
Mykhailo Petrenko <mihalytch@gmail.com>
2018-09-07 14:58:09 +0300

    Init

---
+++ src/main/java/com/luxoft/mapetrenko/restdemoservice/RestDemoServiceApplication.java
@@ -0,0 +1,12 @@
+package com.luxoft.mapetrenko.restdemoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDemoServiceApplication.class, args);
+	}
+}
